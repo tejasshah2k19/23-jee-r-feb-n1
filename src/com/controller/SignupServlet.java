e78f021aa581d5d59d687871d35bbaaf85c8d81e
tejas <tejasshah@gmail.com>
2023-02-04 13:59:36 +0530

    init

---
+++ src/com/controller/SignupServlet.java
@@ -0,0 +1,11 @@
+package com.controller;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class SignupServlet extends HttpServlet {
+	public void service(HttpServletRequest request, HttpServletResponse response) {
+		System.out.println("SignupServlet::service()");
+	}
+}
